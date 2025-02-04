3801cc45ce2fdeb0d2c2bca2de4acf26daddfd33
kalpit10 <kalpit.swami@gmail.com>
2025-02-03 20:19:36 -0500

    Removed the login and register icons

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -37,12 +37,12 @@ function Login() {
   return (
     <div className="login-page">
       <div className="login-box">
-        <div className="illustration-wrapper">
+        {/* <div className="illustration-wrapper">
           <img
             src="https://mixkit.imgix.net/art/preview/mixkit-left-handed-man-sitting-at-a-table-writing-in-a-notebook-27-original-large.png?q=80&auto=format%2Ccompress&h=700"
             alt="Login"
           />
-        </div>
+        </div> */}
         {loading && <Spin size="large" />}
         <Form
           name="login-form"
