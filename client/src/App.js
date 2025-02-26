aeb9e3d9578555581a697bd690fe5267bef83a54
kalpit10 <kalpit.swami@gmail.com>
2025-02-26 15:02:22 -0500

    Updated code quality and pushed latest UI design

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/register";
-import Home from "./pages/home";
+// import Home from "./pages/home";
 import Template from "./pages/template";
 import Profile from "./pages/profile";
 import Templates from "./pages/templates";
@@ -17,8 +17,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/landing" element={<Landing />} />
+          {/* <Route path="/home" element={<Home />} /> */}
+          {/* <Route path="/landing" element={<Landing />} /> */}
           <Route
             path="/template"
             element={
